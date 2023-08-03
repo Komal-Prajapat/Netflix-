124193907198a83540e457dc249fa1a10943b2b8
komalcod <128589948+komalcod@users.noreply.github.com>
2023-08-03 16:15:19 +0530

    first

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>netflix</title>
+
+<link rel="stylesheet" href="css_file.css?v=<?php echo time();?> ">
+
+</head>
+
+
+<div class="demo-wrap">
+  <img
+    class="demo-bg"
+    src="https://assets.digitalocean.com/labs/images/community_bg.png"
+    alt=""
+  >
+  <div class="demo-content">
+ <body >
+
+    <h1 class="logo"><B><I>NETFLIX</B></I></h1>
+    <button class="sign">Sign in </button>
+    <h1 class="data"><b>Unlimited movies, TV shows and more</b></h1><br>
+<h4 >Watch anywhere. Cancel anytime.<BR>
+<BR>
+Ready to watch? Enter your email to create or restart your membership.</h4><BR>
+<input type ="text" class = "email" >
+<button class="get">Get Start></button>
+
+
+</body>
+</html>
